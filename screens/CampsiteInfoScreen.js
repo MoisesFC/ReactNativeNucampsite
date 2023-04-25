79fbea1dcbb900c63d1074afbd8b804ec566a4c8
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-04-24 23:45:07 -0400

    add components, diplay the selected campsite

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,9 @@
+import RenderCampsite from "../features/campsites/RenderCampsites";
+
+const CampsiteInfoScreen = (props) => {
+    return (
+        <RenderCampsite campsite={props.campsite} />
+    )
+};
+
+export default CampsiteInfoScreen;
