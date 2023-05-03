3104edbadf595b827e8db5b5e9f8a8048cdf8993
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-05-03 15:53:40 -0400

    added a sliceReducer to manage state of favorites

--- redux/store.js
+++ redux/store.js
@@ -3,12 +3,14 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/parnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { favoritesReducer } from '../features/favorites/favoritesSlice';
 
 export const store = configureStore({
     reducer: {
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
-        promotions: promotionsReducer
+        promotions: promotionsReducer,
+        favorites: favoritesReducer,
     }
 });
